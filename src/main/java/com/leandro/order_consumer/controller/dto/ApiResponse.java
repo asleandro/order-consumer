792556a5399352a824f245837c85ee77eff82986
Leandro Alves <tk.leandro@gmail.com>
2026-02-27 14:58:39 -0300

    feat: add calculateTotalOrdersValueByCustomerId method

--- src/main/java/com/leandro/order_consumer/controller/dto/ApiResponse.java
+++ src/main/java/com/leandro/order_consumer/controller/dto/ApiResponse.java
@@ -1,7 +1,10 @@
 package com.leandro.order_consumer.controller.dto;
 
 import java.util.List;
+import java.util.Map;
 
-public record ApiResponse<T>(List<T> data, PaginationResponse pagination) {
+public record ApiResponse<T>(Map<String, Object> summary,
+                             List<T> data,
+                             PaginationResponse pagination) {
 
 }
--- src/main/java/com/leandro/order_consumer/service/OrderService.java
+++ src/main/java/com/leandro/order_consumer/service/OrderService.java
@@ -5,20 +5,27 @@ import com.leandro.order_consumer.dto.OrderCreatedEvent;
 import com.leandro.order_consumer.entity.OrderEntity;
 import com.leandro.order_consumer.entity.OrderItem;
 import com.leandro.order_consumer.repository.OrderRepository;
+import org.bson.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event) {
@@ -50,4 +57,15 @@ public class OrderService {
                 .map(i -> new OrderItem(i.produto(), i.quantidade(), i.preco()))
                 .toList();
     }
+
+    public BigDecimal calculateTotalOrdersValueByCustomerId(Long customerId){
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
+    }
+
 }
